f49179a47c0e04efc9d879e2f746dd23bb23ba17
nock <ladrek13@mail.ru>
2023-10-03 18:17:00 +0700

    Добавлен инструмент по получению различных свойств ДНК

--- dna_liv/Processing_DNA/utils/processing_dna.py
+++ dna_liv/Processing_DNA/utils/processing_dna.py
@@ -1,6 +1,49 @@
 
 class ProcessingDna():
 
-    @staticmethod
-    def count_len_dna(dna):
-        return len(dna)
+    def __init__(self, dna: str, features_client):
+        self.dna = dna
+        self.features_client = features_client
+        self.features = {
+            'length': 'Не указано',
+            'cg_count': 'Не указано',
+            'molecular_weight': 'Не указано',
+            'count_nuc': 'Не указано',
+        }
+
+    def count_len_dna(self):
+        self.features['length'] = str(len(self.dna))
+
+    def count_cg(self):
+        count = 0
+        for i in range(len(self.dna) - 1):
+            if self.dna[i:i+2] == 'CG':
+                count += 1
+        percent_cg = count*2/len(self.dna)
+        formatted_average = "{:.1f}".format(percent_cg)  # указываем кол-во знаков после запятой({:.1f})
+        self.features['cg_count'] = formatted_average
+
+    def count_molec_weight(self):
+        weight = 0
+        for i in self.dna:
+            if i == 'T' or i == 'C':
+                weight += 1
+            elif i == 'A' or i == 'T':
+                weight += 2
+        self.features['molecular_weight'] = str(weight)
+        self.features['count_nuc'] = str(52)
+
+    def performing_functions(self):
+        features_handlers_vocab = {
+            'length': self.count_len_dna,
+            'cg_count': self.count_cg,
+            'molecular_weight': self.count_molec_weight,
+        }
+        for el, function in features_handlers_vocab.items():
+            if el in self.features_client:
+                print(el)
+                function()
+        print(self.dna)
+        print(self.features_client)
+        print(self.features)
+        return self.features
--- dna_liv/Processing_DNA/views.py
+++ dna_liv/Processing_DNA/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import DNASequenceForm
 from .utils.processing_dna import ProcessingDna
+from .forms import DNAAnalysisForm
 
 def processing_dna(request):
     return render(request, 'Processing_DNA/index.html')
@@ -11,10 +12,27 @@ def dna_length_add(request):
         if form.is_valid():
             dna_sequence = form.cleaned_data['sequence']
             len_seq = ProcessingDna.count_len_dna(dna_sequence)
-            # Делайте что-то с данными из формы, например, сохраняйте их в базу данных
-            # Затем перенаправьте пользователя на другую страницу или отобразите сообщение об успехе
             return render(request, 'Processing_DNA/add_length.html', {'form': form, 'processed_data': len_seq})
     else:
         form = DNASequenceForm()  # Создаем пустую форму, если это GET запрос
         return render(request, 'Processing_DNA/add_length.html', {'form': form})
     # return render(request, 'Processing_DNA/add_length.html', {'form': form})
+
+def dna_analysis(request):
+    if request.method == 'POST':
+        form = DNAAnalysisForm(request.POST)
+        if form.is_valid():
+            dna_sequence = form.cleaned_data['dna_sequence']
+            selected_properties = form.cleaned_data['properties']
+            processing_dna = ProcessingDna(dna_sequence, selected_properties)
+            result = processing_dna.performing_functions()
+            return render(request, 'Processing_DNA/dna_analysis.html', {'form': form, 'result': result})
+    else:
+        form = DNAAnalysisForm()
+
+    return render(request, 'Processing_DNA/dna_analysis.html', {'form': form})
+
+
+
+
+
